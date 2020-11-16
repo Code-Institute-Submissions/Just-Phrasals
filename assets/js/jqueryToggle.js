db43f0eb274ffc619c4bee14e63b35cc63238867
Jonathan <jonathanmeaney1982@gmail.com>
2020-11-16 13:36:55 +0000

    jqueryToggle.js: semi-colons added

--- assets/js/jqueryToggle.js
+++ assets/js/jqueryToggle.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 // Toggle to show/hide verb example boxes 
 
     $(".verb-box").click(function() {
-        $(this).next().slideToggle("slow")
-    })
+        $(this).next().slideToggle("slow");
+    });
  
 }); 
